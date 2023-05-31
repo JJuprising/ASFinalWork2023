7e3b2e4a987f16dad95a707c033ec21019e25f1b
jybuchiyu <2786087416@qq.com>
2023-05-31 23:20:57 +0800

    Personal界面的标题栏设计

--- app/src/main/java/com/example/asfinalwork2023/ui/personal/PersonalFragment.kt
+++ app/src/main/java/com/example/asfinalwork2023/ui/personal/PersonalFragment.kt
@@ -1,40 +1,29 @@
 package com.example.asfinalwork2023.ui.personal
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import com.example.asfinalwork2023.R
 import com.example.asfinalwork2023.databinding.FragmentPersonalBinding
 
 class PersonalFragment : Fragment() {
 
     private var _binding: FragmentPersonalBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        // 初始化ViewModel，通过ViewModelProvider获取与之对应的数据监听ViewModel，这里是PersonalViewModel
-        val personalViewModel =
-            ViewModelProvider(this).get(PersonalViewModel::class.java)
-
-
+    ): View? {
         _binding = FragmentPersonalBinding.inflate(inflater, container, false)
+        //root为PersonalFragment的根视图
         val root: View = binding.root
-//        val textView: TextView = binding.textPersonal
-//
-//        //在Fragment中观察ViewModel中的LiveData,一旦数据变化,Fragment的UI也会自动更新。这里是textView。
-//        personalViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        val toolbar = root.findViewById<Toolbar>(R.id.personalToolbar)
+        toolbar.title = "Personal"
         return root
     }
 
@@ -42,5 +31,4 @@ class PersonalFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
